48bf95b2155a639c854d8fd4d586886e2c7ef35f
LouBks <louay.belkhamsa@telecom-sudparis.eu>
2025-03-22 17:47:20 +0100

    visitor counter implemented

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("https://your-api-gateway-url.com/visitor-count")
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("visitor-count").innerText = data.count;
-        })
-        .catch(error => console.error("Error fetching visitor count:", error));
-});
+fetch('https://4js2bfq84j.execute-api.eu-west-3.amazonaws.com/production/visitor')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);  
+    document.getElementById("visitorCount").innerText = "Visitor Count: " + data.count;  // Use data.count here
+  })
+  .catch(error => console.error('Error fetching visitor count:', error));
